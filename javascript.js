baa58291891a21b842d8bddf00a64bb041ee10d9
Zven94 <125831572+Zven94@users.noreply.github.com>
2023-05-29 20:01:10 -0300

    fix linters problems

--- javascript.js
+++ javascript.js
@@ -4,25 +4,13 @@ const addBtn = document.querySelector('.add');
 // Create a collection to store books
 let books = [];
 
-// Function to save the collection to localStorage
 function saveBooks() {
   localStorage.setItem('books', JSON.stringify(books));
 }
 
-function removeBook(index) {
-  // Remove the book from the collection using the index
-  books = books.filter((_, i) => i !== index);
-
-  // Save collection to localStorage
-  saveBooks();
-
-  // Display the books
-  displayBooks();
-}
-
 // Function to display all books
 function displayBooks() {
-  // Clear the book list to not reapet the code twice
+  // Clear the book list to not repeat the code twice
   bookList.innerHTML = '';
 
   // Display each book in the collection
@@ -34,22 +22,20 @@ function displayBooks() {
     removeButton.classList.add('removeButtonStyle');
     removeButton.textContent = 'Remove';
     removeButton.addEventListener('click', () => {
-      removeBook(index);
+      // Remove the book from the collection
+      books.splice(index, 1);
+
+      // Save collection to localStorage
+      saveBooks();
+
+      // Display the updated books
+      displayBooks();
     });
 
     listItem.appendChild(removeButton);
-    // insert evey thing befor the input fields
+    // Insert everything before the input fields
     bookList.insertBefore(listItem, bookList.firstChild);
   });
-}
-
-// Function to load the collection from localStorage
-function loadBooks() {
-  const storedBooks = localStorage.getItem('books');
-  if (storedBooks) {
-    books = JSON.parse(storedBooks);
-    displayBooks();
-  }
 }
 
 // Function to add a new book to the collection
@@ -81,6 +67,17 @@ function addBook() {
   authorInput.value = '';
 
   displayBooks();
+}
+
+// Function to save the collection to localStorage
+
+// Function to load the collection from localStorage
+function loadBooks() {
+  const storedBooks = localStorage.getItem('books');
+  if (storedBooks) {
+    books = JSON.parse(storedBooks);
+    displayBooks();
+  }
 }
 
 addBtn.addEventListener('click', addBook);
